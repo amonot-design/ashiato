516ac6f3e3521afa9fedad03c157e6d541b06009
amonot-design <amoso9191@gmail.com>
2026-07-09 13:26:23 +0900

    Add ASHIATO_CONFIG with Supabase credentials

---
+++ config.js
@@ -0,0 +1,4 @@
+window.ASHIATO_CONFIG = {
+  url:     "https://bohbhtwlxvxohkngyjrp.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvaGJodHdseHZ4b2hrbmd5anJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1NTk2MDgsImV4cCI6MjA5OTEzNTYwOH0.xpFlLoyYP48U0i_e3Voznrj97tYO6r96ONqpiSyX4kI"
+};
